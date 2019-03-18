1ef20ee00de3ca5ca73a0a849ec0003f8ab19bad
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-03-18 18:59:39 -0400

    added a dice class for my dice constructor test

---
+++ src/Dice.java
@@ -0,0 +1,12 @@
+
+public class Dice {
+	
+	private boolean isEmpty;
+
+	public void Dice(){
+		this.isEmpty = false;
+	}
+	public Boolean isEmpty(){
+		return this.isEmpty;
+	}
+}
--- src/Test/DiceTest.java
+++ src/Test/DiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import Dice;
+
 public class DiceTest {
 
 	@Test
