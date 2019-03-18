e42a3c95203b41698379a8721ae06fd69ce52a72
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-03-18 19:26:53 -0400

    built a working Player constructor that passes the test

---
+++ src/Game/Player.java
@@ -0,0 +1,15 @@
+package Game;
+
+public class Player {
+	
+	private boolean isEmpty;
+
+	public Player() {
+		this.isEmpty = true;
+	}
+
+	public boolean isEmpty() {
+		return this.isEmpty;
+	}
+
+}
