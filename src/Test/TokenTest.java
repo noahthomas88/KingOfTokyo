289f76bae805103479afa3d3d5ddbecc11de9caf
gearjt <gearjt@rose-hulman.edu>
2019-03-18 19:29:36 -0400

    added failing tests for token types

--- src/Test/TokenTest.java
+++ src/Test/TokenTest.java
@@ -2,14 +2,42 @@ package Test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 import Game.Token;
 
 public class TokenTest {
 
-	@org.junit.Test
-	public void GenericTokenTest() {
+	@Test
+	public void GenericTokenTypeTest() {
 		Token mytoken = new Token();
 		assertEquals(mytoken.type(),"null");
 	}
 
+	@Test
+	public void PoisonTokenTypeTest() {
+		Token mytoken = new PoisonToken();
+		assertEquals(mytoken.type(),"poison");
+	}
+	
+	@Test
+	public void MimicTokenTypeTest() {
+		Token mytoken = new MimicToken();
+		assertEquals(mytoken.type(),"mimic");
+	}
+	
+	@Test
+	public void SmokeTokenTypeTest() {
+		Token mytoken = new SmokeToken();
+		assertEquals(mytoken.type(),"smoke");
+	}
+	
+	@Test
+	public void ShrinkTokenTypeTest() {
+		Token mytoken = new ShrinkToken();
+		assertEquals(mytoken.type(),"shrink");
+	}
+	
+	
+	
 }
