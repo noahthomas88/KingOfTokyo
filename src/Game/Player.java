a1e5deb4c71ea420e20ce006e19cb6c586b3137f
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-03-18 19:55:58 -0400

    modified player to past test

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -5,11 +5,13 @@ public class Player {
 	private boolean isEmpty;
 	public int health;
 	public int victoryPoints;
+	public int energy;
 
 	public Player() {
 		this.isEmpty = false;
 		this.health = 10;
 		this.victoryPoints = 0;
+		this.energy = 0;
 	}
 
 	public boolean isEmpty() {
