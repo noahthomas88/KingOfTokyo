ee2419db674a890df6e2b85c62bcfd7f5556bca7
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-03-18 18:53:19 -0400

    created failing dice constructor test

--- src/Test/DiceTest.java
+++ src/Test/DiceTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class DiceTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void diceConstructorTest() {
+		Dice testDice = new Dice();
+		assertTrue(testDice.isEmpty());
 	}
 
 }
