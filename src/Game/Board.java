5cd0826f7ea93dd93f15559168a910662724247e
Xinyu Shen <shenx@rose-hulman.edu>
2019-03-18 19:38:48 -0400

    testGetNumOfPlayersLB fails

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -4,7 +4,7 @@ public class Board {
 
 	private int numOfPlayers;
 	
-	public Board(int numOfPlayers){
+	public Board(int numOfPlayers) throws IllegalArgumentException{
 		this.numOfPlayers = numOfPlayers;
 	}
 
--- src/Test/BoardTest.java
+++ src/Test/BoardTest.java
@@ -1,9 +1,7 @@
 package Test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import Game.Board;
 
 public class BoardTest {
@@ -12,16 +10,26 @@ public class BoardTest {
 	public void testGetNumOfPlayersmin() {
 		Board b = new Board(2);
 		int num = b.getNumOfPlayers();
-		
+
 		assertEquals(2, num);
 	}
-	
+
 	@Test
 	public void testGetNumOfPlayersmax() {
 		Board b = new Board(6);
 		int num = b.getNumOfPlayers();
-		
+
 		assertEquals(6, num);
+	}
+
+	@Test
+	public void testGetNumOfPlayersLB() {
+		try{
+			Board b = new Board(1);
+			fail("should throw IllegalArgumentException");
+		}catch(IllegalArgumentException e){
+			
+		}
 	}
 
 }
