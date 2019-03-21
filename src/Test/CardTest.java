3be64de6fcdb213c7b53cdfa8e4015b485033014
gearjt <gearjt@rose-hulman.edu>
2019-03-21 19:19:01 -0400

    added failing card test

---
+++ src/Test/CardTest.java
@@ -0,0 +1,16 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardTest {
+
+	@Test
+	public void testConstructor() {
+		Card mycard = new Card();
+		mycard.getType();
+		fail("Not yet implemented");
+	}
+
+}
