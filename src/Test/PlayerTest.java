4528e01accf7de2d56d9666c20d206010552d43b
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-03-21 19:28:01 -0400

    Built failing buildPlayerStatusString test

--- src/Test/PlayerTest.java
+++ src/Test/PlayerTest.java
@@ -27,5 +27,15 @@ public class PlayerTest {
 			
 		}	
 	}
+	
+	@Test
+	public void testBuildPlayerStatusString() {
+		Player player = new Player("TestDummy");
+		String result = player.buildPlayerStatusString();
+		assertTrue(result.equals("name: " + player.name + "/n" 
+							+ "health: " + player.health + "/n"
+							+ "victory points: " + player.victoryPoints + "/n"
+							+ "energy: " + player.energy + "/n"));
+	}
 
 }
