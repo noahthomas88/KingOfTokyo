f81b36618c9cce667461d7063fea9697e7b829df
Xinyu Shen <shenx@rose-hulman.edu>
2019-03-21 15:13:49 -0400

    change the layout and the size for buttons

--- src/Game/GUI.java
+++ src/Game/GUI.java
@@ -2,6 +2,7 @@ package Game;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.LayoutManager;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
@@ -25,7 +26,13 @@ public class GUI {
 	public ArrayList<String> getNames(int numOfPlayers) {
 		ArrayList<String> names = new ArrayList<>();
 		for (int index = 0; index < numOfPlayers; index++) {
-			String name = JOptionPane.showInputDialog("Player #" + (index + 1) + " please enter your name");
+			String name = "";
+			while(name.equals("")){
+				name = JOptionPane.showInputDialog("Player #" + (index + 1) + " please enter your name");
+				if(name.equals("")){
+					JOptionPane.showMessageDialog(null, "name cannot be empty");
+				}
+			}
 			names.add(name);
 		}
 		return names;
@@ -34,29 +41,28 @@ public class GUI {
 	public void displayBoard(Board myBoard) {
 		JFrame myframe = new JFrame();
 		myframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
 		JPanel tokyoPanel = new JPanel();
 		JPanel playerPanel = new JPanel();
-		JButton diebutton = new JButton("roll dice");
-
-		myframe.add(tokyoPanel, BorderLayout.NORTH);
-		myframe.add(playerPanel, BorderLayout.CENTER);
-		myframe.add(diebutton, BorderLayout.EAST);
-
+		JPanel buttonPanel = new JPanel();
+		
 		JButton tokyo = new JButton("Tokyo City");
 		JButton bay = new JButton("Tokyo Bay");
+		JButton diebutton = new JButton("roll dice");
 		tokyoPanel.add(tokyo);
 		tokyoPanel.add(bay);
-
-		tokyo.setPreferredSize(new Dimension(500, 500));
-		bay.setPreferredSize(new Dimension(500, 500));
+		buttonPanel.add(diebutton);
+		
+		tokyo.setPreferredSize(new Dimension(400, 400));
+		bay.setPreferredSize(new Dimension(400, 400));
 		diebutton.setPreferredSize(new Dimension(100, 100));
 
 		ArrayList<JLabel> playertexts = new ArrayList<JLabel>();
 		for (int i = 0; i < 6; i++) {
 			JLabel playertext = new JLabel("empty");
-			playertext.setEnabled(false);
-			playertext.setPreferredSize(new Dimension(200, 200));
+			playertext.setPreferredSize(new Dimension(250, 250));
 			playerPanel.add(playertext);
 			playertexts.add(playertext);
 		}
@@ -64,10 +70,13 @@ public class GUI {
 		for (int i = 0; i < myBoard.player.size(); i++) {
 			JLabel playertoset = playertexts.get(i);
 			playertoset.setText(myBoard.player.get(i).name);
-			playertoset.setEnabled(true);
 		}
 
-		playerPanel.add(diebutton);
+		panel.add(tokyoPanel, BorderLayout.NORTH);
+		panel.add(playerPanel, BorderLayout.CENTER);
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+		
+		myframe.add(panel);
 		myframe.pack();
 		myframe.setVisible(true);
 	}
