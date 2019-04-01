9ca17118b31adfdb104338cd246c22357310d8f6
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-03-31 20:33:48 -0400

    created a high altitude bomber test for the constructir and da HAB class

---
+++ src/CardTests/HighAltitudeBombingTest.java
@@ -0,0 +1,14 @@
+package CardTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HighAltitudeBombingTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
