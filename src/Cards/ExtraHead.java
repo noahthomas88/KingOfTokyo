28fd1bfa17c405286b9e88552ec32ded95c83f15
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-03-31 20:38:33 -0400

    Extra Head passes constructor test

--- src/Cards/ExtraHead.java
+++ src/Cards/ExtraHead.java
@@ -3,7 +3,10 @@ package Cards;
 public class ExtraHead extends Card {
 
 	public ExtraHead() {
-		
+		this.name = "Extra Head";
+		this.cost = 7;
+		this.description = "Gain one extra die";
+		this.type = "Keep";
 	}
 
 }
