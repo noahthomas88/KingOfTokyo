fa0676bc300a5ebfdfdb7912b17138c3ba37d924
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-04-02 14:09:04 -0400

    added GasRefinery test

---
+++ src/CardTests/GasRefineryTest.java
@@ -0,0 +1,14 @@
+package CardTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GasRefineryTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
