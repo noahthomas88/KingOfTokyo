5d1b150622f0f104f2ab015131aa520b0705e53c
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-04-02 13:46:00 -0400

    used teh same TDD process for testing then creating a Ultravore Card

--- src/CardTests/HighAltitudeBombingTest.java
+++ src/CardTests/HighAltitudeBombingTest.java
@@ -5,13 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import Cards.HighAltitudeBombing;
+import Cards.HighAltitudeBombing;
 
 public class HighAltitudeBombingTest {
 
 	@Test
 	public void constructorTest() {
-		HighAltitudeBombing HABcard = new HighAltitudeBombing();
-		assertTrue(HABcard != null);
+		HighAltitudeBombing HighAltitudeBombing = new HighAltitudeBombing();
+		assertTrue(HighAltitudeBombing.name.equals("High Altitude Bombing"));
+		assertTrue(HighAltitudeBombing.description.equals("All monsters including you lose 3 health"));
+		assertTrue(HighAltitudeBombing.type.equals("Discard"));
+		assertTrue(HighAltitudeBombing.cost == 4);
 	}
-
 }
--- src/Cards/HighAltitudeBombing.java
+++ src/Cards/HighAltitudeBombing.java
@@ -1,8 +1,11 @@
 package Cards;
 
-public class HighAltitudeBombing {
+public class HighAltitudeBombing extends Card{
 
 	public HighAltitudeBombing(){
-		
+		this.name = "High Altitude Bombing";
+		this.cost = 4;
+		this.description = "All monsters including you lose 3 health";
+		this.type = "Discard";
 	}
 }
