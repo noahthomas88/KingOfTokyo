5ea6cae45a19da364a47022af89dcb33ccc0763c
Xinyu Shen <shenx@rose-hulman.edu>
2019-04-02 21:27:38 -0400

    refactor DeclConstrctorTest

--- src/cards/DeckConstructor.java
+++ src/cards/DeckConstructor.java
@@ -1,6 +1,7 @@
 package cards;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DeckConstructor {
@@ -13,12 +14,19 @@ public class DeckConstructor {
 	
 	public void createDeck(){
 		deck.add(new ApartmentBuilding());
+		deck.add(new Energize());
 		deck.add(new ExtraHead());
 		deck.add(new GasRefinery());
 		deck.add(new GiantBrain());
 		deck.add(new HighAltitudeBombing());
 		deck.add(new JetFighter());
+		deck.add(new NationalGuard());
+		deck.add(new NuclearPowerPlant());
 		deck.add(new Tank());
 		deck.add(new Ultravore());
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(deck);
 	}
 }
--- test/cards/DeckConstructorTest.java
+++ test/cards/DeckConstructorTest.java
@@ -2,8 +2,6 @@ package cards;
 
 import static org.junit.Assert.*;
 
-import java.util.List;
-
 import org.junit.Test;
 
 public class DeckConstructorTest {
@@ -19,14 +17,13 @@ public class DeckConstructorTest {
 		DeckConstructor d = new DeckConstructor();
 		d.createDeck();
 		assertFalse(d.deck.isEmpty());
-		assertEquals(d.deck.get(0).name, "Apartment Building");
-		assertEquals(d.deck.get(1).name, "Extra Head");
-		assertEquals(d.deck.get(2).name, "Gas Refinery");
-		assertEquals(d.deck.get(3).name, "Giant Brain");
-		assertEquals(d.deck.get(4).name, "High Altitude Bombing");
-		assertEquals(d.deck.get(5).name, "Jet Fighter");
-		assertEquals(d.deck.get(6).name, "Tank");
-		assertEquals(d.deck.get(7).name, "Ultravore");
+		
+		String[] cardNames = {"Apartment Building", "Energize", "Extra Head",
+				"Gas Refinery", "Giant Brain", "High Altitude Bombing",
+				"Jet Fighter", "National Guard", "Nuclear Power Plant", "Tank", "Ultravore"};
+		for (int index = 0; index < cardNames.length; index++) {
+			assertEquals(d.deck.get(index).name, cardNames[index]);
+		}
 	}
 
 }
