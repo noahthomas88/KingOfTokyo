81e4626d3c9a439313c20795802ae3c3fe509bfe
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-04-02 20:38:22 -0400

    built constructor test for tank

---
+++ test/cards/TankTest.java
@@ -0,0 +1,17 @@
+package cards;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TankTest {
+
+	@Test
+	public void constructorTest() {
+		Tank Tank = new Tank();
+		assertTrue(Tank.name.equals("Tank"));
+		assertTrue(Tank.description.equals("Gain 4 Victory Points and Lose 3 Health"));
+		assertTrue(Tank.type.equals("Discard"));
+		assertTrue(Tank.cost == 4);	}
+
+}
