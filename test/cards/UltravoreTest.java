c4c059069dd3de8f0906033efef3dbb256f318fc
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-04-02 20:57:35 -0400

    Built A Constructor Test for Gaint Brain

--- test/cards/UltravoreTest.java
+++ test/cards/UltravoreTest.java
@@ -9,7 +9,7 @@ import cards.Ultravore;
 public class UltravoreTest {
 
 	@Test
-	public void test() {
+	public void constructorTest() {
 		Ultravore Ultravore = new Ultravore();
 		assertTrue(Ultravore.name.equals("Ultravore"));
 		assertTrue(Ultravore.description.equals("Gain 1 extra victory point when beginning your turn in tokyo. If you roll an attack dice in tokyo add an attack to your roll"));
