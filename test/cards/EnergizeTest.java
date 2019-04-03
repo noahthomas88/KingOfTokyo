2fa7826dc58d1b3a67940b26942f1c2cdb44d08f
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-04-02 21:09:42 -0400

    energize constructir test

---
+++ test/cards/EnergizeTest.java
@@ -0,0 +1,14 @@
+package cards;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EnergizeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
