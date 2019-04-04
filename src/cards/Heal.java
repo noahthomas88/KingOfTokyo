e04105c32898fbdd4b1f083bf67510289bd70ad9
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-04-04 11:41:20 -0400

    Heal passes constructor test

---
+++ src/cards/Heal.java
@@ -0,0 +1,12 @@
+package cards;
+
+public class Heal extends Card {
+
+	public Heal() {
+		this.name = "Heal";
+		this.cost = 3;
+		this.description = "Gain 2 Health";
+		this.type = "Discard";
+	}
+
+}
