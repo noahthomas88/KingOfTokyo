910bb4d6bd0ce2ce2d9b8f5e61b8bdd14c9763ec
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-04-04 10:16:07 -0400

    TankLogic passes use test

--- src/cards/CardLogic.java
+++ src/cards/CardLogic.java
@@ -1,6 +1,7 @@
 package cards;
 
 import game.Player;
+
 interface CardLogic {
 
 	void use(Player player);
--- src/cards/TankLogic.java
+++ src/cards/TankLogic.java
@@ -6,7 +6,7 @@ public class TankLogic implements CardLogic {
 
 	public void use(Player p) {
 		p.health -= 3;
-		p.victoryPoints += 0;
+		p.victoryPoints += 4;
 	}
 
 }
