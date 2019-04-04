1b1f714c2955ba0be31f02c4f637315931949a46
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-04-04 19:00:40 -0400

    Flamethrower fails constructor test

---
+++ test/cards/FlameThrower.java
@@ -0,0 +1,18 @@
+package cards;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FlameThrower {
+
+	@Test
+	public void test() {
+		Flamethrower flamethrower = new Flamethrower();
+		assertTrue(flamethrower.name.equals("Flamethrower"));
+		assertTrue(flamethrower.description.equals("All other players lose 2 health"));
+		assertTrue(flamethrower.type.equals("Discard"));
+		assertTrue(flamethrower.cost == 3);
+	}
+
+}
