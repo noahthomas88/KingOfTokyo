2ed193d196e6b9189b11383cd13557b6072fc88b
Joshua Warning <warninjd@warninjd-5520.rose-hulman.edu>
2019-04-04 15:47:20 -0400

    Evacuation Orders pass constructor tests

---
+++ src/cards/EvacuationOrders.java
@@ -0,0 +1,12 @@
+package cards;
+
+public class EvacuationOrders extends Card {
+
+	public EvacuationOrders() {
+		this.name = "Evacuation Orders";
+		this.cost = 7;
+		this.description = "All other monsters lose 5 Victory Points";
+		this.type = "Discard";
+	}
+
+}
--- test/cards/EvacuationOrdersTest.java
+++ test/cards/EvacuationOrdersTest.java
@@ -10,7 +10,8 @@ public class EvacuationOrdersTest {
 	public void constructorTest() {
 		EvacuationOrders evac = new EvacuationOrders();
 		assertTrue(evac.name.equals("Evacuation Orders"));
-		assertTrue(evac.description.equals("All other monsters lose 5 victory points"));
+		assertTrue(evac.description.equals("All other monsters lose "
+				+ "5 Victory Points"));
 		assertTrue(evac.type.equals("Discard"));
 		assertTrue(evac.cost == 7);
 	}
