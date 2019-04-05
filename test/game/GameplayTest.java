2e86818b099ff199caf400d4658d7186d0ef9fcd
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-04-04 21:15:11 -0400

    fix gameplaytest

--- test/game/GameplayTest.java
+++ test/game/GameplayTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package game;
 
 import static org.junit.Assert.*;
@@ -95,6 +94,13 @@ public class GameplayTest {
 		gameplay.endTurn();
 	}
 	
+	
+	/*
+	 *TODO: Split this out into seperate cases with each dice roll effect and test to see if players
+	 *internal attributes are modified 
+	 *
+	 *Ex: rolled 1 heal? check to see that a players heal went up by 1
+	 */
 	@Test
 	public void DiceRolledTest() {
 		GUI gui = EasyMock.createNiceMock(GUI.class);
@@ -167,132 +173,3 @@ public class GameplayTest {
 	
 
 }
-=======
-//package game;
-//
-//import static org.junit.Assert.*;
-//
-//import java.util.ArrayList;
-//
-//import org.easymock.EasyMock;
-//import org.junit.Test;
-//
-//import game.Gameplay;
-//import main.GUI;
-//
-//public class GameplayTest {
-//	
-//	@Test
-//	public void GameplayInitializationTest() {
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		EasyMock.replay(gui);
-//		Gameplay gameplay = new Gameplay(gui);
-//		gameplay.initializeGame();
-//		assertTrue(gameplay.gameUI==gui);
-//		EasyMock.verify(gui);
-//	}
-//
-//	@Test
-//	public void PlayerSelectionTest() {
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		EasyMock.replay(gui);
-//		Gameplay gameplay = new Gameplay(gui);
-//		gameplay.initializeGame();
-//		assertTrue(gameplay.currentplayer == null);
-//		gameplay.selectFirstPlayer();
-//		assertTrue(gameplay.currentplayer!=null);
-//		EasyMock.verify(gui);
-//	}
-//	
-//	@Test
-//	public void BeginGameTest() {
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		EasyMock.replay(gui);
-//		Gameplay gameplay = new Gameplay(gui);
-//		gameplay.initializeGame();
-//		gameplay.beginGame();
-//		EasyMock.verify(gui);
-//	}
-//	
-//	@Test
-//	public void BeginTurnTest() {
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		EasyMock.replay(gui);
-//		Gameplay gameplay = new Gameplay(gui);
-//		gameplay.beginTurn();
-//		EasyMock.verify(gui);
-//	}
-//	
-//	@Test
-//	public void EndTurnTest() {
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		Gameplay gameplay = new Gameplay(gui);
-//		gameplay.endTurn();
-//	}
-//	
-//	@Test
-//	public void DiceRolledTest() {
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		Gameplay gameplay = new Gameplay(gui);
-//		ArrayList<Dice> dicelist = EasyMock.createNiceMock(ArrayList.class);
-//		gameplay.diceRolled(dicelist);
-//	}
-//	
-//	@Test
-//	public void CalculateScoreTest() {
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		Gameplay gameplay = new Gameplay(gui);
-//		ArrayList<Dice> dicelist = EasyMock.createNiceMock(ArrayList.class);
-//		gameplay.calculateScore(dicelist);
-//	}
-//	
-//	@Test
-//	public void CedeTokyoTest() {
-//		GUI gui = EasyMock.createNiceMock(GUI.class);
-//		ArrayList<String> fakenames = new ArrayList<String>();
-//		fakenames.add("player1");
-//		fakenames.add("player2");
-//		EasyMock.expect(gui.getNumPlayers()).andReturn(2);
-//		EasyMock.expect(gui.getNames(2)).andReturn(fakenames);
-//		Gameplay gameplay = new Gameplay(gui);
-//		gameplay.cedeTokyo();
-//	}
-//	
-//
-//}
->>>>>>> 1877aea2c43a88707744bf5a430b50a35b1a573e
