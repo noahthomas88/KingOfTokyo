1ccc600f7cc8ed1b4376b89e7df39cb654a60ece
gearjt <gearjt@rose-hulman.edu>
2019-04-17 19:23:44 -0400

    removed junk code in preparation for mass refactoring

--- src/game/Gameplay.java
+++ src/game/Gameplay.java
@@ -13,168 +13,53 @@ import main.GUI;
 public class Gameplay {
 
 	public Player currentplayer;
-	public Board gameboard;
 	public GUI gameUI;
-	DeckConstructor deck = new DeckConstructor();
-	HashMap<String, Integer> playerToNumber = new HashMap<String, Integer>();
 
 	public Gameplay(GUI gui) {
-		this.gameUI = gui;
+		
 	}
 
 	public void initializeGame() {
 
-		int numOfPlayers;
-
-		try {
-			numOfPlayers = gameUI.getNumPlayers();
-			gameboard = new Board(numOfPlayers);
-		} catch (IllegalArgumentException e) {
-			JOptionPane.showMessageDialog(null, "Number of players have to be between 2 to 6");
-			return;
-		}
-
-		gameboard.constructPlayers(gameUI.getNames(numOfPlayers));
-		for (int i = 0; i < gameboard.playerList.size(); i++) {
-			playerToNumber.put(gameboard.playerList.get(i).name, i);
-		}
-		deck.createDeck();
-		deck.reveal();
-		gameUI.displayBoard(gameboard, numOfPlayers, this);
-		gameUI.setCards(deck.visibleCard);
 	}
 
 	public void beginGame() {
-		selectFirstPlayer();
-		gameUI.displayStartingPlayer(currentplayer.name);
-		beginTurn();
 
-	}
-
-	public void runCardEffects() {
-		System.out.println("running card effects");
 	}
 
 	public void beginTurn() {
-		gameUI.setActivePlayer(playerToNumber.get(currentplayer.name));
-		gameUI.DisableEndTurnButton();
-		if (gameboard.cityPlayer == currentplayer) {
-			currentplayer.addVictory(2);
-		}
-		if (gameboard.cityPlayer == null) {
-			gameboard.cityPlayer = currentplayer;
-			gameUI.moveToTokyo(currentplayer);
-			currentplayer.addVictory(1);
-		}
-		gameUI.updatePlayerText(gameboard);
-		gameUI.DisableCedeButton();
-		runCardEffects();
+	
 	}
 
 	public void diceRolled(ArrayList<Dice> dicelist) {
-		ArrayList<Dice> otherdice = new ArrayList<Dice>();
-		for (Dice die : dicelist) {
-			String result = die.numberToString(die.numberRolled);
-			if (result.equals("attack")) {
-				gameboard.doAttack(currentplayer);
-				gameUI.EnableCedeButton();
-			} else if (result.equals("heal") && currentplayer != gameboard.cityPlayer) {
-				currentplayer.addHealth(1);
-			} else if (result.equals("energy")) {
-				currentplayer.addEnergy(1);
-			} else {
-				otherdice.add(die);
-			}
-		}
-		calculateScore(otherdice);
-		gameUI.EnableEndTurnButton();
-		gameUI.updatePlayerText(gameboard);
+
 	}
 
 	public void calculateScore(ArrayList<Dice> dice) {
-		int count3 = 0;
-		int count2 = 0;
-		int count1 = 0;
-		for (Dice die : dice) {
-			if (die.numberRolled == 1) {
-				count1++;
-			} else if (die.numberRolled == 2) {
-				count2++;
-			} else {
-				count3++;
-			}
-		}
-		if (count3 > 2) {
-			currentplayer.addVictory(3 + (count3 - 3));
-		}
-		if (count2 > 2) {
-			currentplayer.addVictory(2 + (count2 - 3));
-		}
-		if (count1 > 2) {
-			currentplayer.addVictory(1 + (count1 - 3));
-		}
+
 	}
 
 	public void endTurn() {
-		if (playerToNumber.get(currentplayer.name) >= (gameboard.numOfPlayers - 1)) {
-			currentplayer = gameboard.playerList.get(0);
-		} else {
-			currentplayer = gameboard.playerList.get(playerToNumber.get(currentplayer.name) + 1);
-		}
-		beginTurn();
+
 	}
 	
 	public void buyCard(int number) {
-		Card tobuy = deck.visibleCard[number-1];
-		if(currentplayer.energy>=tobuy.cost) {
-			currentplayer.cardsInHand.add(deck.visibleCard[number-1]);
-			currentplayer.addEnergy(-tobuy.cost);
-			deck.buy(number-1);
-			gameUI.setCards(deck.visibleCard);
-			gameUI.updatePlayerText(gameboard);
-		} else {
-			gameUI.energyWarning();
-		}
+
 	}
 
 	public void selectFirstPlayer() {
-		ArrayList<Player> playerlist = gameboard.playerList;
-		Integer numOfPlayers = gameboard.numOfPlayers;
-		Random random = new Random();
-		Integer firstplayer = random.nextInt(numOfPlayers);
-		this.currentplayer = playerlist.get(firstplayer);
+
 	}
 
 	public void cedeTokyo() {
-		gameboard.cityPlayer = currentplayer;
-		gameUI.moveToTokyo(currentplayer);
-		currentplayer.addVictory(1);
-		gameUI.DisableCedeButton();
+
 	}
 
 	public void swipeCard() {
-		if(currentplayer.energy>=2) {
-			deck.swipe();
-			gameUI.setCards(deck.visibleCard);
-			currentplayer.addEnergy(-2);
-		} else {
-			gameUI.energyWarning();
-		}
+
 	}
 
 	public void useCard(String cardname) {
-		Card touse = null;
-		for(Card card : currentplayer.cardsInHand) {
-			if(card.name.equals(cardname)) {
-				touse = card;
-			}
-		}
-		if(touse!=null) {
-			touse.logic.use(currentplayer);
-			if(touse.type.equals("Discard")) {
-				currentplayer.cardsInHand.remove(touse);
-			}
-		}
 		
 	}
 
