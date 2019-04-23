f6ff487d4cc39d83f9f579f7cd1ef3d3c7923680
gearjt <gearjt@rose-hulman.edu>
2019-04-23 19:21:28 -0400

    added card information before buying

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -45,10 +45,32 @@ public class GUI {
 			cardbutton.setText(card.name);
 			cardbutton.setPreferredSize(new Dimension(200,200));
 			description.setPreferredSize(new Dimension(200,200));
+			cardbutton.addActionListener(new UseCardListener(card.name));
 			panel.add(cardbutton, BorderLayout.CENTER);
 			panel.add(description,BorderLayout.SOUTH);
 		}
 		JOptionPane.showConfirmDialog(null, panel, "Here is your hand",JOptionPane.DEFAULT_OPTION);
+	}
+	
+	public void viewCard(int index) {
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		JLabel label2 = new JLabel();
+		JTextField description = new JTextField();
+		panel.setLayout(new BorderLayout());
+		Card card = game.deck.visibleCard[index-1];
+		description.setText(card.description);
+		description.setEditable(false);
+		label.setText("Cost: " + card.cost + " energy");
+		label2.setText(card.name);
+		description.setPreferredSize(new Dimension(200,200));
+		panel.add(description, BorderLayout.CENTER);
+		panel.add(label, BorderLayout.SOUTH);
+		panel.add(label2, BorderLayout.NORTH);
+		int option = JOptionPane.showConfirmDialog(null, panel, "Here is your hand",JOptionPane.OK_CANCEL_OPTION);
+		if(option == JOptionPane.OK_OPTION) {
+			game.buyCard(index);
+		}
 	}
 	
 	public void setCards(Card[] cards) {
@@ -323,7 +345,7 @@ public class GUI {
 		
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			game.buyCard(1);
+			viewCard(1);
 		}
 	}
 	
@@ -331,7 +353,7 @@ public class GUI {
 		
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			game.buyCard(2);
+			viewCard(2);
 		}
 	}
 	
@@ -339,7 +361,7 @@ public class GUI {
 		
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			game.buyCard(3);
+			viewCard(3);
 		}
 	}
 	
