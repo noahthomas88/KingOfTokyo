80d92d0da671586de5f59675a2858b873660bb86
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-05-03 09:11:59 -0400

    added logic class to stretchy test

--- test/cards/StretchyTest.java
+++ test/cards/StretchyTest.java
@@ -13,6 +13,7 @@ public class StretchyTest {
 		assertTrue(stretchy.description.equals("Gain 4 Victory Points"));
 		assertTrue(stretchy.type.equals("Discard"));
 		assertTrue(stretchy.cost == 6);
+		assertTrue(stretchy.logic.getClass().equals(StretchyLogic.class));
 	}
 
 }
