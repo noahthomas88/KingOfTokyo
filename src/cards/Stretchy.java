affce7f626dec3dfe05ee1e7db4023f2755b4195
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-05-03 09:11:15 -0400

    added to stretchy class, test now passes

---
+++ src/cards/Stretchy.java
@@ -0,0 +1,12 @@
+package cards;
+
+public class Stretchy extends Card{
+
+	public Stretchy(){
+		this.name = "Stretchy";
+		this.cost = 3;
+		this.description = "Before resolving your dice you can spend 2 energy to change any of your dice to any result";
+		this.type = "Keep";
+	}	
+	
+}
--- test/cards/StretchyTest.java
+++ test/cards/StretchyTest.java
@@ -8,6 +8,7 @@ public class StretchyTest {
 
 	@Test
 	public void test() {
+		Stretchy stretchy = new Stretchy();
 		assertTrue(stretchy.name.equals("stretchy"));
 		assertTrue(stretchy.description.equals("Gain 4 Victory Points"));
 		assertTrue(stretchy.type.equals("Discard"));
