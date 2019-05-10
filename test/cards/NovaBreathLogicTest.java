9839d063b28bb18ac41b2cad5d21657507e3429b
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-05-09 23:24:05 -0400

    test to see that atttack affects all players passes

---
+++ test/cards/NovaBreathLogicTest.java
@@ -0,0 +1,34 @@
+package cards;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import game.Board;
+import game.Dice;
+import game.Player;
+
+public class NovaBreathLogicTest {
+
+	@Test
+	public void testAddTwoHealthHaveCard() {
+		Player player = new Player("test");
+		Player player2 = new Player("test");
+		ArrayList<Player> playerList = new ArrayList<Player>();
+		playerList.add(player);
+		playerList.add(player2);
+		
+		Card card = EasyMock.strictMock(Card.class);
+		card.name = "NovaBreath";
+		Board b = new Board(3);
+		b.playerList = playerList;
+		player2.health = 10;
+		player.addToHand(card);
+		b.doAttack(player, 1);
+		assertEquals(player2.health, 9);
+	}
+
+}
