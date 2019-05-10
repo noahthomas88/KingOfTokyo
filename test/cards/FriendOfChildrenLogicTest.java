d8580e125e827fac456c51ca2b7fccabf569d83c
Xinyu Shen <shenx@rose-hulman.edu>
2019-05-10 02:58:32 -0400

    remove FriendOfChildrenLogic class

--- test/cards/FriendOfChildrenLogicTest.java
+++ test/cards/FriendOfChildrenLogicTest.java
@@ -8,18 +8,6 @@ import org.junit.Test;
 import game.Player;
 
 public class FriendOfChildrenLogicTest {
-
-	@Test(expected = UnsupportedOperationException.class)
-	public void testUse() {
-		FriendOfChildrenLogic logic = new FriendOfChildrenLogic();
-		logic.use(null);
-	}
-	
-	@Test(expected = UnsupportedOperationException.class)
-	public void testUse2() {
-		FriendOfChildrenLogic logic = new FriendOfChildrenLogic();
-		logic.use(null, null);
-	}
 	
 	@Test
 	public void testAddOneEnergyHaveCard() {
