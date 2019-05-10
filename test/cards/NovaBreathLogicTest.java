4c325b9608009bf1159196e2deb051ce5ef58fc0
Xinyu Shen <shenx@rose-hulman.edu>
2019-05-10 03:01:11 -0400

    tests for Nova Breath fails

--- test/cards/NovaBreathLogicTest.java
+++ test/cards/NovaBreathLogicTest.java
@@ -14,21 +14,90 @@ import game.Player;
 public class NovaBreathLogicTest {
 
 	@Test
-	public void testAttackEffectsAllPlayers() {
-		Player player = new Player("test");
-		Player player2 = new Player("test");
+	public void testCityAttack() {
 		ArrayList<Player> playerList = new ArrayList<Player>();
-		playerList.add(player);
-		playerList.add(player2);
-		
-		Card card = EasyMock.strictMock(Card.class);
-		card.name = "NovaBreath";
+		Player p1, p2, p3, p4;
+		playerList.add(p1 = new Player("test1"));
+		playerList.add(p2 = new Player("test2"));
+		playerList.add(p3 = new Player("test3"));
+		playerList.add(p4 = new Player("test4"));
+
 		Board b = new Board(3);
 		b.playerList = playerList;
-		player2.health = 10;
-		player.addToHand(card);
-		b.doAttack(player, 1);
-		assertEquals(player2.health, 9);
+		b.cityPlayer = p1;
+		b.bayPlayer = p2;
+		
+		Card card = new Card();
+		card.name = "Nova Breath";
+		p1.addToHand(card);
+		
+		p1.health = 10;
+		p2.health = 10;
+		p3.health = 10;
+		p4.health = 10;
+		
+		b.doAttack(p1, -3);
+		assertEquals(p2.health, 7);
+		assertEquals(p3.health, 7);
+		assertEquals(p4.health, 7);
+	}
+	
+	@Test
+	public void testBayAttack() {
+		ArrayList<Player> playerList = new ArrayList<Player>();
+		Player p1, p2, p3, p4;
+		playerList.add(p1 = new Player("test1"));
+		playerList.add(p2 = new Player("test2"));
+		playerList.add(p3 = new Player("test3"));
+		playerList.add(p4 = new Player("test4"));
+
+		Board b = new Board(3);
+		b.playerList = playerList;
+		b.cityPlayer = p1;
+		b.bayPlayer = p2;
+		
+		Card card = new Card();
+		card.name = "Nova Breath";
+		p2.addToHand(card);
+		
+		p1.health = 10;
+		p2.health = 10;
+		p3.health = 10;
+		p4.health = 10;
+		
+		b.doAttack(p2, -3);
+		assertEquals(p1.health, 7);
+		assertEquals(p3.health, 7);
+		assertEquals(p4.health, 7);
+	}
+	
+	@Test
+	public void testOutAttack() {
+		ArrayList<Player> playerList = new ArrayList<Player>();
+		Player p1, p2, p3, p4;
+		playerList.add(p1 = new Player("test1"));
+		playerList.add(p2 = new Player("test2"));
+		playerList.add(p3 = new Player("test3"));
+		playerList.add(p4 = new Player("test4"));
+
+		Board b = new Board(3);
+		b.playerList = playerList;
+		b.cityPlayer = p1;
+		b.bayPlayer = p2;
+		
+		Card card = new Card();
+		card.name = "Nova Breath";
+		p3.addToHand(card);
+		
+		p1.health = 10;
+		p2.health = 10;
+		p3.health = 10;
+		p4.health = 10;
+		
+		b.doAttack(p3, -3);
+		assertEquals(p2.health, 7);
+		assertEquals(p1.health, 7);
+		assertEquals(p4.health, 7);
 	}
 
 }
