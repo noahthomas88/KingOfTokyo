d153c5b47ca2d2b7afeb9e6eed4f0af06b933a34
Xinyu Shen <shenx@rose-hulman.edu>
2019-05-10 03:02:09 -0400

    add an edge case where card effect is not used

--- test/cards/RootingForTheUnderdogLogicTest.java
+++ test/cards/RootingForTheUnderdogLogicTest.java
@@ -34,6 +34,30 @@ public class RootingForTheUnderdogLogicTest {
 			
 	}
 	
+	@Test
+	public void rootingForTheUnderdogLogicNoEffectTest() {
+		Player p = new Player("NewPlayer");
+		Player p2 = new Player("NewPlayer2");
+		Player p3 = new Player("NewPlayer3");
+		
+		p.victoryPoints = 13;
+		p2.victoryPoints = 11;
+		p3.victoryPoints = 12;
+		
+		ArrayList<Player> players = new ArrayList<>();
+		players.add(p);
+		players.add(p2);
+		players.add(p3);
+		
+		RootingForTheUnderdogLogic rootingForTheUnderdogLogic = new RootingForTheUnderdogLogic();
+		rootingForTheUnderdogLogic.use(p, players);
+		
+		assertTrue(p.victoryPoints == 13);
+		assertTrue(p2.victoryPoints == 11);
+		assertTrue(p3.victoryPoints == 12);
+			
+	}
+	
 	@Test(expected = UnsupportedOperationException.class)
 	public void testUse() {
 		RootingForTheUnderdogLogic logic = new RootingForTheUnderdogLogic();
