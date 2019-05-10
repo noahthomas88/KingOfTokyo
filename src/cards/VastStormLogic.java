1594e852624c2de324b689a7c327e97a776b3577
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-05-09 23:01:13 -0400

    resolved merge conflict with vast storm

--- src/cards/VastStormLogic.java
+++ src/cards/VastStormLogic.java
@@ -13,25 +13,26 @@ public class VastStormLogic implements CardLogic {
 
 	@Override
 	public void use(Player player, ArrayList<Player> players) {
-		
-		for(int i = 0; i < players.size(); i++) {
+
+		for (int i = 0; i < players.size(); i++) {
 			int energyHalved = players.get(i).energy;
-			if(energyHalved % 2 != 0) {
-				energyHalved = (energyHalved/2) + 1;
+			if (energyHalved % 2 != 0) {
+				energyHalved = (energyHalved / 2) + 1;
 			} else {
-			if(energyHalved % 2 == 0) {
-				energyHalved = energyHalved/2;
-			} else {
-				energyHalved = (energyHalved/2) + 1;
+				if (energyHalved % 2 == 0) {
+					energyHalved = energyHalved / 2;
+				} else {
+					energyHalved = (energyHalved / 2) + 1;
+				}
+				if (!players.get(i).equals(player)) {
+					players.get(i).energy = energyHalved;
+				} else {
+					players.get(i).addEnergy(2);
+				}
+
 			}
-			if(!players.get(i).equals(player)) {
-				players.get(i).energy = energyHalved;
-			} else {
-				players.get(i).addEnergy(2);
-			}
-			
+
 		}
 
 	}
-
 }
