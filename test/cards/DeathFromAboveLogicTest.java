213717e58c28f66a26057ceb84e01869dc26bb01
Xinyu Shen <shenx@rose-hulman.edu>
2019-05-11 20:14:19 -0400

    Death from above logic test fail

--- test/cards/DeathFromAboveLogicTest.java
+++ test/cards/DeathFromAboveLogicTest.java
@@ -2,13 +2,38 @@ package cards;
 
 import static org.junit.Assert.*;
 
+import org.easymock.EasyMock;
 import org.junit.Test;
+
+import game.Player;
+import main.GUI;
 
 public class DeathFromAboveLogicTest {
 
-	@Test
+	@Test(expected = UnsupportedOperationException.class)
 	public void testUse() {
-		fail("Not yet implemented");
+		DeathFromAboveLogic logic = new DeathFromAboveLogic();
+		logic.use(null);
+	}
+	
+	@Test
+	public void useCard() {
+		DeathFromAboveLogic logic = new DeathFromAboveLogic();
+		Card card = EasyMock.strictMock(Card.class);
+		Player player = EasyMock.strictMock(Player.class);
+		GUI gui = EasyMock.strictMock(GUI.class);
+		
+		player.addToHand(card);
+		player.addVictory(2);
+		gui.moveToTokyo(player);
+		
+		EasyMock.replay(player, gui);
+		
+		card.logic = logic;
+		player.addToHand(card);
+		logic.use(player, null, gui);
+		
+		EasyMock.verify(player, gui);
 	}
 
 }
