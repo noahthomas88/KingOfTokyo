608c2cbb7842548848373877b43918d8941a040f
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-05-19 11:54:48 -0400

    Detrivore logic test added

--- test/cards/DetrivoreLogicTest.java
+++ test/cards/DetrivoreLogicTest.java
@@ -13,32 +13,25 @@ import game.Gameplay;
 import game.Player;
 import main.Messages;
 
-public class AcidAttackLogicTest {
+public class DetrivoreLogicTest {
 
 	@Test
-	public void addsOneAttackTest() {
+	public void addsTwoVictoryPointsTest() {
 		Player player = new Player("test");
 		Player attackedPlayer = new Player("test");
-		attackedPlayer.health = 2;
+		attackedPlayer.victoryPoints = 0;
 		Card card = EasyMock.strictMock(Card.class);
 		card.name = "Acid Attack";
 		Gameplay gameplay = EasyMock.mock(Gameplay.class);
 		ArrayList<Dice> dice = new ArrayList<Dice>();
 		Messages message = EasyMock.mock(Messages.class);
 		Board board = EasyMock.mock(Board.class);
-		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(player);
-		players.add(attackedPlayer);
-		board.playerList  = players;
-		board.cityPlayer = player;
 		gameplay.gameboard = board;
 		gameplay.currentplayer = player;
 		
 		gameplay.diceRolled(dice, message);
 		
-		assertEquals(1,attackedPlayer.health);
-	
-		
+		assertEquals(2,player.victoryPoints);
 	}
 
 }
