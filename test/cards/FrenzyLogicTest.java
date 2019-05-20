7047b3f81b9f6a3f0c6a8aff3653e9c550b165d4
Thomas, Noah B <thomasnb@rose-hulman.edu>
2019-05-19 23:11:07 -0400

    Frenzy Logic test added

---
+++ test/cards/FrenzyLogicTest.java
@@ -0,0 +1,45 @@
+package cards;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import game.Board;
+import game.Dice;
+import game.Gameplay;
+import game.Player;
+import main.GUI;
+import main.Messages;
+
+public class FrenzyLogicTest {
+
+	@Test
+	public void GetAnotherTurnTest() {
+		Player player = new Player("test");
+		player.victoryPoints = 0;
+		Card card = new Card();
+		card.name = "Frenzy";
+		player.addToHand(card);
+
+		ArrayList<Dice> dice = new ArrayList<Dice>();
+		Messages message = EasyMock.niceMock(Messages.class);
+		Board board = new Board(2, null, null);
+		GUI gui = EasyMock.niceMock(GUI.class);
+		Gameplay gameplay = new Gameplay(gui, player, board, null, null);
+
+		Dice oneDie = new Dice(gameplay.currentplayer);
+
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(player);
+		board.playerList = players;
+		board.cityPlayer = player;
+
+		gameplay.diceRolled(dice, message);
+
+		assertEquals(player, gameplay.currentplayer);
+	}
+
+}
